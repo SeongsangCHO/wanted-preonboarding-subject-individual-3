4850bc92c84096e08b810d8a83dc9246da05c2c2
Seongsangcho <liasqui31@gmail.com>
2021-08-20 19:43:29 +0900

    feat : filter기능 추가 구현

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Modal as ModalComponent, Button } from "antd";
+import { Modal as ModalComponent } from "antd";
 
 interface ModalProps {
   isModalOpen?: boolean;
